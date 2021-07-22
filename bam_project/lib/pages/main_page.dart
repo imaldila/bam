2515292a8d231dac897667e783455a6eeaf7859b
aldilanurhadiputra@gmail.com <aldilanurhadiputra@gmail.com>
2021-07-22 18:43:50 +0700

    Splash Screen
    Add First Splash Screen

---
+++ bam_project/lib/pages/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
