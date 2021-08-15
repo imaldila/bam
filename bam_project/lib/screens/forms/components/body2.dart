6bddaa5da95bea5857901c3c6640ed7cad76fd9a
Aldila Nurhadiputra <aldilanurhadiputra@gmail.com>
2021-08-15 16:54:33 +0700

    icon launcher
    Add new icon launcher

---
+++ bam_project/lib/screens/forms/components/body2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Body2 extends StatelessWidget {
+  const Body2({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
